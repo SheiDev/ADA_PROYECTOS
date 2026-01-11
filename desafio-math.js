83b116e0345b11da4593454cf123595b0727eff6
Sheik-la <waithsheila@gmail.com>
2026-01-11 15:01:59 -0500

    Subir ADA_PROYECTOS completo

---
+++ desafio-math.js
@@ -0,0 +1,8 @@
+// Consigna:
+// Dadas las temperaturas de la semana, calcular cuál fue la temperatura máxima.
+
+const temperaturas = [12, 18, 39, 22, 30];
+
+const temperaturaMaxima = Math.max(...temperaturas);
+
+console.log(temperaturaMaxima);
